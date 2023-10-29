6a58d8d9483d2ce896b1ac3e1494c47eb8025d22
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-10-29 11:18:27 -0400

    Forgot @apollo/server: ^4.7.1 in server package.json

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 
 // This for testing queries with Apollo every time the server runs
@@ -9,7 +8,7 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3011;
 const app = express();
 
 
