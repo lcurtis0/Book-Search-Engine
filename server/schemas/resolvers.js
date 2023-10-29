7d02374963845eaa1eea1bee87d40aaa50467910
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-10-28 20:22:21 -0400

    Finished resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,6 +3,10 @@
 
 // This will be used for the google api for books
 
+// This file was heavily reference by activity 25
+
+
+
 const { Book, User } = require('../models');
 
 const resolvers = {
@@ -16,6 +20,73 @@ const resolvers = {
         },
     },
     Mutation: {
-        
+        addProfile: async (parent /* Note: Parent may not be necessary */, args) => {
+            const profile = await Profile.create({ name, email, password });
+            const token = signToken(profile);
+
+            return { token, profile };
+        },
+        loginUser: async (parent, { email, password }) => {
+            const profile = await Profile.findOne({ email });
+
+            if (!profile) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await profile.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(profile);
+            return { token, profile };
+        },
+
+        // This section is for adding a book either loggedin or not
+
+        addBook: async (parent, { profileId, book }, context) => {
+            // If context has a `user` property, meaning if the user is loggedin, this will trigger this will assign a new book
+            if (context.user) {
+                return Profile.findOneAndUpdate(
+                    {
+                        _id: profileId
+                    },
+                    {
+                        $addToSet: { books: book },
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                );
+            }
+            // If user asks to add a book and isn't logged in, it will throw an error
+            throw AuthenticationError;
+        },
+
+        // This section is for delete an account (profile)
+
+        removeProfile: async (parent, args, context) => {
+            if (context.user) {
+                return Profile.findOneAndDelete({ _id: context.user._id });
+            }
+            throw AuthenticationError;
+        },
+
+        // This section is for deleting a existing book
+
+        // This can only work if the user is logged in
+
+        removeBook: async (parent, { book }, context) => {
+            if (context.user) {
+                return Profile.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { books: book } },
+                    { new: true }
+                );
+            }
+            throw AuthenticationError;
+        },
     }
 }
